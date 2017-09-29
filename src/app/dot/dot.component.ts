15f95b5c28f901fd2ab60fb4be77dbddfd9da86b
gund <malkevich.alex@gmail.com>
2017-09-29 21:39:56 +0400

    feat(dot): Implement dot component

--- src/app/dot/dot.component.ts
+++ src/app/dot/dot.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-dot',
   templateUrl: './dot.component.html',
-  styleUrls: ['./dot.component.css']
+  styleUrls: ['./dot.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DotComponent implements OnInit {
+export class DotComponent implements OnChanges {
 
-  constructor() { }
+  @Input() x: number;
+  @Input() y: number;
+  @Input() size: number;
+  @Input() text: string;
 
-  ngOnInit() {
+  hover = false;
+  style: { [k: string]: any };
+  bgColor: string;
+
+  ngOnChanges(changes: SimpleChanges) {
+    const shapeChanged = 'x' in changes || 'y' in changes || 'size' in changes;
+
+    if (shapeChanged) {
+      this.updateStyle();
+    }
+  }
+
+  enter() {
+    this.hover = true;
+    this.bgColor = '#ff0';
+  }
+
+  leave() {
+    this.hover = false;
+    this.bgColor = undefined;
+  }
+
+  private updateStyle() {
+    const s = this.size * 1.3;
+    this.style = {
+      width: s + 'px',
+      height: s + 'px',
+      left: (this.x) + 'px',
+      top: (this.y) + 'px',
+      borderRadius: (s / 2) + 'px',
+      lineHeight: (s) + 'px',
+    };
   }
 
 }
