ad89b04a9fe6915c35b5aadf722fbdb6a746be9d
gund <malkevich.alex@gmail.com>
2017-09-30 02:18:49 +0400

    fix(ex): Rebuild examples for github pages

--- scripts/update-ex.js
+++ scripts/update-ex.js
@@ -2,15 +2,15 @@
 
 const { readFileSync, writeFileSync } = require('fs');
 
-const SINGLE_HTML_FILE = 'example/single/index.html';
-const WORKER_HTML_FILE = 'example/worker/index.html';
+const SINGLE_HTML_FILE = 'docs/single/index.html';
+const WORKER_HTML_FILE = 'docs/worker/index.html';
 
 const singleHtml = readFileSync(SINGLE_HTML_FILE, 'utf-8');
 const workerHtml = readFileSync(WORKER_HTML_FILE, 'utf-8');
 
 console.log('Updating base urls...');
-writeFileSync(SINGLE_HTML_FILE, updateBase(singleHtml, '/single/'));
-writeFileSync(WORKER_HTML_FILE, updateBase(workerHtml, '/worker/'));
+writeFileSync(SINGLE_HTML_FILE, updateBase(singleHtml, '/ng-s-triangle-demo/single/'));
+writeFileSync(WORKER_HTML_FILE, updateBase(workerHtml, '/ng-s-triangle-demo/worker/'));
 console.log('Done');
 
 /**
