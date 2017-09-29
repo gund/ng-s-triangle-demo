50d1eda191849a7929b4cb02be089254e72070a0
gund <malkevich.alex@gmail.com>
2017-09-29 21:40:26 +0400

    feat(app): Render test 20x20 dots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  title = 'app';
+  size = 20;
+  sizeS = this.size + 5;
+  items = new Array(20).fill(new Array(20));
 }
